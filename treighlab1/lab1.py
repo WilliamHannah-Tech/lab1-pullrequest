13a192e6c8c476be21dfcc6dcc1b9fa915dd9ae0
Treigh-Tech <232130879+Treigh-Tech@users.noreply.github.com>
2026-01-11 13:52:33 -0700

    treighnyc

---
+++ treighlab1/lab1.py
@@ -0,0 +1,11 @@
+first_number = input ("Enter the first number: ")
+second_number = input ("Enter the second number: ")
+
+numb1 = int(first_number)
+numb2 = int(second_number)
+
+sum_value = numb1 + numb2
+
+print("thinking...")
+
+print(f"the sum of {numb1} and {numb2} is {sum_value}")
